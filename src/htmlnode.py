6ba42ed9d74d7f3d3f001dd37ef246d9bd3ee018
Tim Lin <81815607+TimmyLin21@users.noreply.github.com>
2026-02-28 21:43:46 +1100

    Add parent and leaf nodes

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -16,4 +16,29 @@ class HTMLNode:
     def __repr__(self):
         return f"tag={self.tag} value={self.value} children={self.children} props={self.props}"
 
-    
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("All leaf nodes must have a value")
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"tag={self.tag} value={self.value} props={self.props}"
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("All parent nodes must have a tag")
+        if self.children is None:
+            raise ValueError("All parent nodes must have children")
+        return f"<{self.tag}{self.props_to_html()}>{
+            "".join(list(map(lambda child: child.to_html(),self.children)))
+        }</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html(self):
@@ -24,5 +24,39 @@ class TestHTMLNode(unittest.TestCase):
         node = HTMLNode()
         self.assertEqual(node.tag, None)
 
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_repr(self):
+        div = LeafNode("div","This is div",props={
+            "class": "flex"
+        })
+        self.assertEqual(repr(div), "tag=div value=This is div props={'class': 'flex'}")
+
+class TestParentNode(unittest.TestCase):
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
+
+    def test_to_html_with_multichildren(self):
+        child_node = LeafNode("span", "child")
+        child_node2 = LeafNode("span", "child2")
+        parent_node = ParentNode("div", [child_node, child_node2])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span>child</span><span>child2</span></div>",
+        )
 if __name__ == "__main__":
     unittest.main()
