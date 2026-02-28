e8f08bd6895c7925d0ee55e9f8da39c9c1dbd8a5
Tim Lin <81815607+TimmyLin21@users.noreply.github.com>
2026-02-28 22:23:20 +1100

    Add text to html

--- src/main.py
+++ src/main.py
@@ -1,4 +1,25 @@
 from textnode import TextNode, TextType
+from htmlnode import LeafNode
+
+def text_node_to_html_node(text_node):
+    if text_node.text_type not in TextType:
+        raise Exception("Text type is invalid")
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(None,text_node.text)
+        case TextType.BOLD:
+            return LeafNode("b",text_node.text)
+        case TextType.ITALIC:
+            return LeafNode("i",text_node.text)
+        case TextType.CODE:
+            return LeafNode("code",text_node.text)
+        case TextType.LINK:
+            return LeafNode("a",text_node.text,{"href":text_node.url})
+        case TextType.IMAGE:
+            return LeafNode("img","",{"src":text_node.url, "alt": text_node.text})
+        
+
+
 
 def main():
     link = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
